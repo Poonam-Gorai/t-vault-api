2da5eb9927f3cb7e1f577784348a38dcd729c8fa
Poonam-Gorai <poonamgorai1807@gmail.com>
2021-10-18 14:36:08 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app =  express()
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const routesURls = require('./routes/route')
+const cors =  require('cors')
+
+dotenv.config()
+
+mongoose.connect(process.env.DATABASE_ACCESS,() =>console.log("DataBase connected"))
+
+// app.get('/',(req,res)=>{
+//     res.send("we are here")
+// })
+
+app.use(express.json())
+app.use(cors())
+app.use('/',routesURls)
+app.listen(3000,() => console.log("server is up and running"))
